721ef2edc04913cfa7e983ddc220333e8f83e0c6
cristodh <cdelgado.hidalgo1996fwd@gmail.com>
2025-11-14 16:45:11 -0600

    arreglos registro

--- FE/proyect_fe/src/Register/components/RegisFormD/RegisFormD.jsx
+++ FE/proyect_fe/src/Register/components/RegisFormD/RegisFormD.jsx
@@ -14,15 +14,23 @@ import {
 import { Form } from 'react-router-dom';
 
 export default function RegisFormD() {
-  const [checkEducation, setCheckEducation] =useState(true);
-  const [checkHealth, setCheckHealth] =useState(false);
-  const [checkEnvironment, setCheckEnvironment] =useState(false);
-  const [checkCommunityDevelopment, setCheckCommunityDevelopment] =useState(false);
-  const [checkScienceandTechnology, setCheckScienceandTechnology] =useState(false);
-  const [checkArtsandCulture, setCheckArtsandCulture] =useState(false);
-  const [checkAnimalWelfare, setCheckAnimalWelfare] =useState(false);
-  const [checkSportsandRecreation, setCheckSportsandRecreation] =useState(false);
-  const [checkOther, setCheckOther] =useState(false);
+  const [checkEducation, setCheckEducation] = useState(false);
+  const [checkHealth, setCheckHealth] = useState(false);
+  const [checkEnvironment, setCheckEnvironment] = useState(false);
+  const [checkCommunityDevelopment, setCheckCommunityDevelopment] = useState(false);
+  const [checkScienceandTechnology, setCheckScienceandTechnology] = useState(false);
+  const [checkArtsandCulture, setCheckArtsandCulture] = useState(false);
+  const [checkAnimalWelfare, setCheckAnimalWelfare] = useState(false);
+  const [checkSportsandRecreation, setCheckSportsandRecreation] = useState(false);
+  const [checkOther, setCheckOther] = useState(false);
+  const [interests, setInterests] = useState([]);
+  const interestArray = []
+
+  const addInterest = (interest) => {
+    interestArray.push(interest);
+    setInterests([...interestArray, interest]);
+    console.log(interestArray);
+  }
 
   return (
     <form className="register-form-d">
@@ -52,15 +60,70 @@ export default function RegisFormD() {
         <div className="interest-group">
           <label className="interest-label">Áreas de interés</label>
           <FormGroup row>
-            <FormControlLabel control={<Checkbox checked={checkEducation} />} label="Educación" />
-            <FormControlLabel control={<Checkbox checked={checkHealth} />} label="Salud" />
-            <FormControlLabel control={<Checkbox checked={checkEnvironment} />} label="Medio ambiente" />
-            <FormControlLabel control={<Checkbox checked={checkCommunityDevelopment} />} label="Desarrollo de la Comunidad" />
-            <FormControlLabel control={<Checkbox checked={checkScienceandTechnology} />} label="Ciencia y Tecnología" />
-            <FormControlLabel control={<Checkbox checked={checkArtsandCulture} />} label="Arte y cultura" />
-            <FormControlLabel control={<Checkbox checked={checkAnimalWelfare} />} label="Protección Animal" />
-            <FormControlLabel control={<Checkbox checked={checkSportsandRecreation} />} label="Deporte y Recreacion" />
-            <FormControlLabel control={<Checkbox checked={checkOther} />} label="Otros" />
+            <FormControlLabel control={<Checkbox
+              onClick={() => {
+                setCheckEducation(!checkEducation);
+                addInterest("Education")
+              }}
+              checked={checkEducation}
+
+            />} label="Educación" />
+            <FormControlLabel control={<Checkbox 
+            onClick={()=>{
+              setCheckHealth(!checkHealth);
+              addInterest("Health")
+            }}
+            checked={checkHealth} 
+            />} label="Salud" />
+            <FormControlLabel control={<Checkbox
+             checked={checkEnvironment} 
+              onClick={()=>{
+                setCheckEnvironment(!checkEnvironment);
+                addInterest("Environment")
+              }}
+             />} label="Medio ambiente" />
+            <FormControlLabel control={<Checkbox 
+            checked={checkCommunityDevelopment}
+            onClick={()=>{
+              setCheckCommunityDevelopment(!checkCommunityDevelopment);
+              addInterest("Community Development")
+            }}
+            />} label="Desarrollo de la Comunidad" />
+            <FormControlLabel control={<Checkbox 
+            checked={checkScienceandTechnology}
+            onClick={()=>{
+              setCheckScienceandTechnology(!checkScienceandTechnology);
+              addInterest("Science and Technology")
+            }}
+            />} label="Ciencia y Tecnología" />
+            <FormControlLabel control={<Checkbox 
+            checked={checkArtsandCulture}
+            onClick={()=>{
+              setCheckArtsandCulture(!checkArtsandCulture);
+              addInterest("Arts and Culture")
+            }}
+            />} label="Arte y cultura" />
+            <FormControlLabel control={<Checkbox
+             checked={checkAnimalWelfare}
+             onClick={()=>{
+              setCheckAnimalWelfare(!checkAnimalWelfare);
+              addInterest("Animal Welfare")
+            }}
+             />} label="Protección Animal" />
+            <FormControlLabel control={<Checkbox 
+            checked={checkSportsandRecreation}
+            onClick={()=>{
+              setCheckSportsandRecreation(!checkSportsandRecreation);
+              addInterest("Sports and Recreation")
+            }}
+            />} label="Deporte y Recreacion" />
+            <FormControlLabel control={<Checkbox 
+            checked={checkOther}
+            onClick={()=>{
+              setCheckOther(!checkOther);
+              addInterest("Other")
+            }}
+            />} label="Otros" />
             {/* <TextField label="Otros" variant="outlined" */}
           </FormGroup>
         </div>
